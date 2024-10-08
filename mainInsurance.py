72c3745741de478a8c0b28e03b86fe37e153e43a
mohamedZK00 <163613527+mohamedZK00@users.noreply.github.com>
2024-10-09 00:29:37 +0300

    Update mainInsurance.py

--- mainInsurance.py
+++ mainInsurance.py
@@ -29,6 +29,15 @@ except Exception as e:
         print(f"Failed to load model with joblib: {e}")
         raise e  # رفع الاستثناء إذا لم يتم التحميل
 
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class InsurancePRED(BaseModel):
     age: int
     sex: str
